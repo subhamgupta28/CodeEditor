33369d21bd88fb72c2b4032b40db4132f39acfe1
= <67224573+subhamgupta28@users.noreply.github.com>
2025-04-28 13:19:55 +0530

    init

--- src/main/java/dev/subhamgupta/codeeditor/controller/CodeController.java
+++ src/main/java/dev/subhamgupta/codeeditor/controller/CodeController.java
@@ -1,5 +1,7 @@
 package dev.subhamgupta.codeeditor.controller;
 
+import dev.subhamgupta.codeeditor.services.CodeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import java.nio.file.*;
@@ -8,56 +10,38 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/code")
+@RequiredArgsConstructor
 @CrossOrigin(origins = "*") // Allow React frontend
 public class CodeController {
 
-    private final Path workspaceDir = Paths.get("workspace"); // relative to project root
+    private final CodeService codeService;
+    // relative to project root
 
     @PostMapping("/save")
     public ResponseEntity<?> saveCode(@RequestBody Map<String, String> body) throws IOException {
-        String code = body.get("code");
-        String filePath = body.get("file");
-        Files.createDirectories(workspaceDir.resolve(filePath).getParent());
-        Files.writeString(workspaceDir.resolve(filePath), code);
+        codeService.saveCode(body);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/build")
     public ResponseEntity<?> build() throws IOException, InterruptedException {
-        ProcessBuilder builder = new ProcessBuilder("platformio", "run");
-        builder.directory(workspaceDir.toFile());
-        Process process = builder.start();
-        int exitCode = process.waitFor();
-        return ResponseEntity.ok(Map.of("exitCode", exitCode));
+        return ResponseEntity.ok(codeService.build());
     }
 
     @PostMapping("/upload")
     public ResponseEntity<?> upload() throws IOException, InterruptedException {
-        ProcessBuilder builder = new ProcessBuilder("platformio", "run", "--target", "upload");
-        builder.directory(workspaceDir.toFile());
-        Process process = builder.start();
-        int exitCode = process.waitFor();
-        return ResponseEntity.ok(Map.of("exitCode", exitCode));
+
+        return ResponseEntity.ok(codeService.upload());
     }
 
     @GetMapping("/list")
     public ResponseEntity<?> listFiles() throws IOException {
-        List<Map<String, Object>> files = new ArrayList<>();
-        Files.walk(workspaceDir)
-                .filter(Files::isRegularFile)
-                .forEach(path -> {
-                    Map<String, Object> file = new HashMap<>();
-                    file.put("name", workspaceDir.relativize(path).toString());
-                    file.put("isDirectory", Files.isDirectory(path));
-                    files.add(file);
-                });
-        return ResponseEntity.ok(files);
+
+        return ResponseEntity.ok(codeService.listFiles());
     }
 
     @GetMapping("/open")
     public ResponseEntity<?> openFile(@RequestParam String file) throws IOException {
-        Path filePath = workspaceDir.resolve(file);
-        String content = Files.readString(filePath);
-        return ResponseEntity.ok(Map.of("content", content));
+        return ResponseEntity.ok(codeService.openFile(file));
     }
 }
--- src/main/java/dev/subhamgupta/codeeditor/services/CodeService.java
+++ src/main/java/dev/subhamgupta/codeeditor/services/CodeService.java
@@ -1,4 +1,62 @@
 package dev.subhamgupta.codeeditor.services;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
 public class CodeService {
+
+    private final Path workspaceDir = Paths.get("/home/subham/arduino-projects");
+
+    public void saveCode(Map<String, String> body) throws IOException {
+        String code = body.get("code");
+        String filePath = body.get("file");
+        Files.createDirectories(workspaceDir.resolve(filePath).getParent());
+        Files.writeString(workspaceDir.resolve(filePath), code);
+    }
+
+    public Map<String, Integer> build() throws IOException, InterruptedException{
+        ProcessBuilder builder = new ProcessBuilder("platformio", "run");
+        builder.directory(workspaceDir.toFile());
+        Process process = builder.start();
+        int exitCode = process.waitFor();
+        return Map.of("exitCode", exitCode);
+    }
+
+    public Object upload() throws IOException, InterruptedException{
+        ProcessBuilder builder = new ProcessBuilder("platformio", "run", "--target", "upload");
+        builder.directory(workspaceDir.toFile());
+        Process process = builder.start();
+        int exitCode = process.waitFor();
+        return Map.of("exitCode", exitCode);
+    }
+
+    public List<Map<String, Object>> listFiles() throws IOException {
+        List<Map<String, Object>> files = new ArrayList<>();
+        Files.walk(workspaceDir)
+//                .filter(Files::isRegularFile)
+                .forEach(path -> {
+                    Map<String, Object> file = new HashMap<>();
+                    file.put("name", workspaceDir.relativize(path).toString());
+                    file.put("isDirectory", Files.isDirectory(path));
+                    files.add(file);
+                });
+        return files;
+    }
+
+    public Map<String, String> openFile(String file) throws IOException {
+        Path filePath = workspaceDir.resolve(file);
+        String content = Files.readString(filePath);
+        return Map.of("content", content);
+    }
 }
